1019b2152b7f36e0b6a762eb0386e94d55fe4459
doraemonsk8ers <doraemon.sk8ers@gmail.com>
2014-03-08 13:02:48 +0800

    Updates

---
+++ CRXDownloader/main.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CRXDownloader
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(51, 13);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(251, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(308, 13);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL:";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 52);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chrome Extension Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CRXDownloader/main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace CRXDownloader
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            char[] delimiter = new char[] { '/' };
+            char[] delimiter2 = new char[] { '?' };
+            string szPath = txtURL.Text;
+            string[] tokens = szPath.Split(delimiter, StringSplitOptions.RemoveEmptyEntries);
+            int iLen = tokens.Length;
+            string[] szExtensionID = tokens[tokens.Length - 1].Split(delimiter2, StringSplitOptions.None);
+            string szDownloadPath = "https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D" + szExtensionID[0] + "%26uc";
+            WebClient webClient = new WebClient();
+            string szFile = tokens[tokens.Length-2] + ".crx";
+            webClient.DownloadFile(szDownloadPath, szFile);
+            MessageBox.Show("Download Completed.");
+        }
+    }
+}
